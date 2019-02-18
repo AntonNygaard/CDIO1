7e24d84c5cce444d190e2088af828f4fa029e380
Anton <s170429@student.dtu.dk>
2019-02-18 17:00:38 +0100

    rebase

--- CDIO1/src/main/java/dal/DAO.java
+++ CDIO1/src/main/java/dal/DAO.java
@@ -10,19 +10,62 @@ public class DAO implements IUserDAO {
     Scanner s = new Scanner(System.in);
 
     @Override
-    public UserDTO getUser(int userId) throws DALException {
+    public void getUser(int userId) {
+        userdto.toString(userId);
+    }
+
+    @Override
+    public List<UserDTO> getUserList() {
         return null;
     }
 
     @Override
-    public List<UserDTO> getUserList() throws DALException {
-        return null;
-    }
-
-    @Override
-    public void createUser(UserDTO user) throws DALException {
+    public void createUser() {
         System.out.println("Indtast userID:");
         int newUserId = s.nextInt();
+        userdto.setUserId(newUserId);
+
+        System.out.println("Indtast username:");
+        String newUserName = s.next();
+        userdto.setUserName(newUserId,newUserName);
+
+        System.out.println("Indtast initialer:");
+        String newIni = "abcdefg";
+        while(newIni.length() > 4 || newIni.length() < 1) {
+            newIni = s.next();
+            if (newIni.length() > 4 && newIni.length() < 1) {
+                System.out.println("Dine initialer skal være mellem 1 og 4 tegn, prøv igen");
+            }
+            else {
+                userdto.setIni(newUserId,newIni);
+            }
+        }
+
+        System.out.println("Indtast dit cpr nummer");
+        String newCpr = "a";
+        while (newCpr.length() != 10) {
+            newCpr = s.next();
+            if (newCpr.length() != 10) {
+                System.out.println("Et cpr nummer er 10 tegn langt, prøv igen");
+            }
+            else {
+                userdto.setCpr(newUserId,newCpr);
+            }
+        }
+
+        // TODO: Lav random password generator:
+        System.out.println("Indtast password:");
+        String newPassword = "abc";
+        while(newPassword.length() < 6 && newPassword.length() > 50) {
+            newPassword = s.next();
+            if (newPassword.length() < 6 && newPassword.length() > 50) {
+                System.out.println("Dit password skal være mellem 6 og 50 tegn, prøv igen");
+            }
+            else {
+                userdto.setPassword(newUserId,newPassword);
+            }
+        }
+
 
     }
 
--- CDIO1/src/main/java/dto/UserDTO.java
+++ CDIO1/src/main/java/dto/UserDTO.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD:CDIO1/src/main/java/dto/UserDTO.java
 package dto;
 
 import java.io.Serializable;
@@ -110,3 +111,191 @@ public class UserDTO implements Serializable{
 	
 	
 }
+=======
+package dto;
+
+import java.io.Serializable;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserDTO implements Serializable{
+
+	private static final long serialVersionUID = 4545864587995944260L;
+	private List<String> roles;
+	//TODO Add relevant fields
+
+
+	public UserDTO() {
+		this.roles = new ArrayList<>();
+	}
+
+	public int getUserId(int userId) {
+	    // TODO´´
+        try (Connection connection = DriverManager.getConnection("jdbc:mysql://ec2-52-30-211-3.eu-west-1.compute.amazonaws.com/s170429?"
+                + "user=s170429&password=4PPt5j8rsEIUnrBq8G0iE")){
+            Statement statement = connection.createStatement();
+            String getSQL_ID = String.format("SELECT userid FROM users WHERE userid='%s'",userId);
+            ResultSet SQL_ID = statement.executeQuery(getSQL_ID);
+            while (SQL_ID.next()){
+                int get_userID = SQL_ID.getInt("userid");
+                return get_userID;
+            }
+
+        } catch (SQLException e) {
+            //Remember to handle Exceptions gracefully! Connection might be Lost....
+            e.printStackTrace();
+        }
+        return 0;
+	}
+	public void setUserId(int userId) {
+        try (Connection connection = DriverManager.getConnection("jdbc:mysql://ec2-52-30-211-3.eu-west-1.compute.amazonaws.com/s170429?"
+                + "user=s170429&password=4PPt5j8rsEIUnrBq8G0iE")) {
+            Statement statement = connection.createStatement();
+            String addSQL_ID = String.format("INSERT INTO users (userid) VALUES ('%s');", userId);
+            statement.executeUpdate(addSQL_ID);
+
+        } catch (SQLException e) {
+            //Remember to handle Exceptions gracefully! Connection might be Lost....
+            e.printStackTrace();
+        }
+    }
+	public String getUserName(int userId) {
+            try (Connection connection = DriverManager.getConnection("jdbc:mysql://ec2-52-30-211-3.eu-west-1.compute.amazonaws.com/s170429?"
+                    + "user=s170429&password=4PPt5j8rsEIUnrBq8G0iE")){
+                Statement statement = connection.createStatement();
+                String getSQL_UserName = String.format("SELECT userName FROM users WHERE userid='%s'",userId);
+                ResultSet SQL_UserName = statement.executeQuery(getSQL_UserName);
+                while (SQL_UserName.next()){
+                    String get_UserName = SQL_UserName.getString("userName");
+                    return get_UserName;
+                }
+
+            } catch (SQLException e) {
+                //Remember to handle Exceptions gracefully! Connection might be Lost....
+                e.printStackTrace();
+            }
+            return "Fejl";
+	}
+	public void setUserName(int userId, String userName) {
+        try (Connection connection = DriverManager.getConnection("jdbc:mysql://ec2-52-30-211-3.eu-west-1.compute.amazonaws.com/s170429?"
+                + "user=s170429&password=4PPt5j8rsEIUnrBq8G0iE")) {
+            Statement statement = connection.createStatement();
+            String addSQL_ID = String.format("UPDATE users SET userName='%s' WHERE userid='%s';", userName,userId);
+            statement.executeUpdate(addSQL_ID);
+        } catch (SQLException e) {
+            //Remember to handle Exceptions gracefully! Connection might be Lost....
+            e.printStackTrace();
+        }
+    }
+	public String getIni(int userId) {
+        try (Connection connection = DriverManager.getConnection("jdbc:mysql://ec2-52-30-211-3.eu-west-1.compute.amazonaws.com/s170429?"
+                + "user=s170429&password=4PPt5j8rsEIUnrBq8G0iE")){
+            Statement statement = connection.createStatement();
+            String getSQL_Ini = String.format("SELECT ini FROM users WHERE userid='%s'",userId);
+            ResultSet SQL_Ini = statement.executeQuery(getSQL_Ini);
+            while (SQL_Ini.next()){
+                String get_Ini = SQL_Ini.getString("ini");
+                return get_Ini;
+            }
+
+        } catch (SQLException e) {
+            //Remember to handle Exceptions gracefully! Connection might be Lost....
+            e.printStackTrace();
+        }
+        return "Fejl";
+	}
+	public void setIni(int userId, String ini) {
+        try (Connection connection = DriverManager.getConnection("jdbc:mysql://ec2-52-30-211-3.eu-west-1.compute.amazonaws.com/s170429?"
+                + "user=s170429&password=4PPt5j8rsEIUnrBq8G0iE")) {
+            Statement statement = connection.createStatement();
+            String addSQL_Ini = String.format("UPDATE users SET ini='%s' WHERE userid='%s';", ini,userId);
+            statement.executeUpdate(addSQL_Ini);
+        } catch (SQLException e) {
+            //Remember to handle Exceptions gracefully! Connection might be Lost....
+            e.printStackTrace();
+        }
+	}
+    public String getCpr(int userId) {
+        try (Connection connection = DriverManager.getConnection("jdbc:mysql://ec2-52-30-211-3.eu-west-1.compute.amazonaws.com/s170429?"
+                + "user=s170429&password=4PPt5j8rsEIUnrBq8G0iE")){
+            Statement statement = connection.createStatement();
+            String getSQL_Cpr = String.format("SELECT cpr FROM users WHERE userid='%s'",userId);
+            ResultSet SQL_Cpr = statement.executeQuery(getSQL_Cpr);
+            while (SQL_Cpr.next()){
+                String get_Cpr = SQL_Cpr.getString("cpr");
+                return get_Cpr;
+            }
+
+        } catch (SQLException e) {
+            //Remember to handle Exceptions gracefully! Connection might be Lost....
+            e.printStackTrace();
+
+        }
+        return "Fejl";
+    }
+    public void setCpr(int userId, String cpr) {
+        try (Connection connection = DriverManager.getConnection("jdbc:mysql://ec2-52-30-211-3.eu-west-1.compute.amazonaws.com/s170429?"
+                + "user=s170429&password=4PPt5j8rsEIUnrBq8G0iE")) {
+            Statement statement = connection.createStatement();
+            String addSQL_Cpr = String.format("UPDATE users SET cpr='%s' WHERE userid='%s';",cpr, userId);
+            statement.executeUpdate(addSQL_Cpr);
+        } catch (SQLException e) {
+            //Remember to handle Exceptions gracefully! Connection might be Lost....
+            e.printStackTrace();
+        }
+    }
+    public String getPassword(int userId) {
+        try (Connection connection = DriverManager.getConnection("jdbc:mysql://ec2-52-30-211-3.eu-west-1.compute.amazonaws.com/s170429?"
+                + "user=s170429&password=4PPt5j8rsEIUnrBq8G0iE")){
+            Statement statement = connection.createStatement();
+            String getSQL_Password = String.format("SELECT password FROM users WHERE userid='%s'",userId);
+            ResultSet SQL_Password = statement.executeQuery(getSQL_Password);
+            while (SQL_Password.next()){
+                String get_Password = SQL_Password.getString("userPassword");
+                return get_Password;
+            }
+
+        } catch (SQLException e) {
+            //Remember to handle Exceptions gracefully! Connection might be Lost....
+            e.printStackTrace();
+
+        }
+        return "Fejl";
+    }
+    public void setPassword(int userId,String password) {
+        try (Connection connection = DriverManager.getConnection("jdbc:mysql://ec2-52-30-211-3.eu-west-1.compute.amazonaws.com/s170429?"
+                + "user=s170429&password=4PPt5j8rsEIUnrBq8G0iE")) {
+            Statement statement = connection.createStatement();
+            String addSQL_Password = String.format("UPDATE users SET userPassword='%s' WHERE userid='%s';", password,userId);
+            statement.executeUpdate(addSQL_Password);
+        } catch (SQLException e) {
+            //Remember to handle Exceptions gracefully! Connection might be Lost....
+            e.printStackTrace();
+        }
+    }
+    public void getRoles() {
+	}
+	public void setRoles(List<String> roles) {
+		this.roles = roles;
+	}
+	public void addRole(String role){
+		this.roles.add(role);
+	}
+	/**
+	 *
+	 * @param role
+	 * @return true if role existed, false if not
+	 */
+	public boolean removeRole(String role){
+		return this.roles.remove(role);
+	}
+
+	public String toString(int userId) {
+		return "UserDTO [userId=" + getUserId(userId) + ", userName=" + getUserName(userId) + ", ini=" + getIni(userId) + "]";
+	}
+
+
+
+}
+>>>>>>> Fikset diverse:CDIO1/src/dto/UserDTO.java
