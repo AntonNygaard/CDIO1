c2e6b8eb6cd4912f5f597294356f564dcae248e9
Anton <s170429@student.dtu.dk>
2019-02-18 16:31:10 +0100

    Revert "Fikset password create bug"
    This reverts commit 277d18579ccab797279a208ded6f6dbd33f25bad.

--- CDIO1/src/Menu.java
+++ CDIO1/src/Menu.java
@@ -9,7 +9,6 @@ public class Menu {
         System.out.println("-----------------------");
         System.out.println("1 Tilføj bruger");
         System.out.println("2 Se bruger");
-        System.out.println("3 Opdater bruger");
 
         System.out.print("Indtast valg: ");
         int menuItem = s.nextInt();
@@ -25,7 +24,6 @@ public class Menu {
                 dao.getUser(getUserDataID);
                 break;
             case 3:
-            case 4:
                 System.exit(0);
                 break;
             default:
